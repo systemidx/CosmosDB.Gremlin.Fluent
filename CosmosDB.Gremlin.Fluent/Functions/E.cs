db9cc09db6a33e37f2f7aa477b62be435876b7e5
Bryon Weber <bryon.weber@gmail.com>
2018-09-17 16:38:47 -0400

    Adds support for E()

---
+++ CosmosDB.Gremlin.Fluent/Functions/E.cs
@@ -0,0 +1,15 @@
+﻿using CosmosDB.Gremlin.Fluent;
+
+namespace CosmosDB.Gremlin.Fluent.Functions
+{
+    public static class EFunction
+    { 
+        public static GremlinQueryBuilder V(this GremlinQueryBuilder builder, GremlinParameter value = null)
+        {
+            if (value == null)
+                return builder.Add($"E()");
+
+            return builder.Add($"E({value.Value})");
+        }
+    }
+}
