5e37671a96cf1749e74b4d48c7c4246009c0afdb
Bryon Weber <bryon.weber@gmail.com>
2018-09-17 18:07:58 -0400

    Adds support for where()

---
+++ CosmosDB.Gremlin.Fluent/Functions/Where.cs
@@ -0,0 +1,10 @@
+﻿namespace CosmosDB.Gremlin.Fluent.Functions
+{
+    public static class WhereFunction
+    {
+        public static GremlinQueryBuilder Where(this GremlinQueryBuilder builder, params GremlinQueryBuilder[] functions)
+        {
+            return builder.Add($"where({functions.Expand()})");
+        }
+    }
+}
