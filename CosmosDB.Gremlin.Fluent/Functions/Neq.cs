05097fa5b4e0f8b4ad93fbf76d24df6fb7ef6d67
Bryon Weber <bryon.weber@gmail.com>
2018-09-18 10:09:39 -0400

    Adds support for neq() with parameters

--- CosmosDB.Gremlin.Fluent/Functions/Neq.cs
+++ CosmosDB.Gremlin.Fluent/Functions/Neq.cs
@@ -6,5 +6,10 @@
         {
             return builder.Add($"neq({inner.Query})");
         }
+
+        public static GremlinQueryBuilder Neq(this GremlinQueryBuilder builder, GremlinParameter parameter)
+        {
+            return builder.Add($"neq({parameter.Value})");
+        }
     }
 }
