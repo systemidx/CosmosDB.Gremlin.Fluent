61aa26787af3356aacfb4a4065090ffa99c4250c
Bryon Weber <bryon.weber@gmail.com>
2018-09-18 09:56:51 -0400

    Adds support for neq()

---
+++ CosmosDB.Gremlin.Fluent/Functions/Neq.cs
@@ -0,0 +1,10 @@
+﻿namespace CosmosDB.Gremlin.Fluent.Functions
+{
+    public static class NeqFunction
+    {
+        public static GremlinQueryBuilder Neq(this GremlinQueryBuilder builder, GremlinQueryBuilder inner)
+        {
+            return builder.Add($"neq({inner.Query})");
+        }
+    }
+}
