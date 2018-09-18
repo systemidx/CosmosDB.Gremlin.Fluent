8d460d6683395ed0c0994c5ff7e889c0386b6e47
Bryon Weber <bryon.weber@gmail.com>
2018-09-17 22:05:02 -0400

    Adds support for drop()

---
+++ CosmosDB.Gremlin.Fluent/Functions/Drop.cs
@@ -0,0 +1,10 @@
+﻿namespace CosmosDB.Gremlin.Fluent.Functions
+{
+    public static class DropFunction
+    {
+        public static GremlinQueryBuilder Drop(this GremlinQueryBuilder builder)
+        {
+            return builder.Add("drop()");
+        }
+    }
+}
