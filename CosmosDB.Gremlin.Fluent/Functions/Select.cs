60b53dfaf3831310aed927be3760554e937bc909
Bryon Weber <bryon.weber@gmail.com>
2018-09-18 09:50:40 -0400

    Adds support for select()

---
+++ CosmosDB.Gremlin.Fluent/Functions/Select.cs
@@ -0,0 +1,10 @@
+﻿namespace CosmosDB.Gremlin.Fluent.Functions
+{
+    public static class SelectFunction
+    {
+        public static GremlinQueryBuilder Select(this GremlinQueryBuilder builder, params GremlinParameter[] parameters)
+        {
+            return builder.Add($"select({parameters.Expand()})");
+        }
+    }
+}
