57d36f5fd4c6db94c088eb3afabdea53553f82ba
Aieat Assam <Aieat.Assam@epicor.com>
2019-08-01 16:23:38 +0100

    Added store

---
+++ CosmosDB.Gremlin.Fluent/Functions/Store.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace CosmosDB.Gremlin.Fluent.Functions
+{
+    public static class StoreFunction
+    {
+        public static GremlinQueryBuilder Store(this GremlinQueryBuilder builder, IGremlinParameter parameter)
+        {
+            if (parameter == null)
+                throw new ArgumentNullException(nameof(parameter));
+            
+            builder.AddArgument(parameter as GremlinArgument);
+            return builder.Add($"store({parameter.Value})");
+        }
+        
+        // for implicit conversion operators
+        public static GremlinQueryBuilder Store(this GremlinQueryBuilder builder, GremlinParameter parameter)
+        {
+            return builder.Store((IGremlinParameter)parameter);
+        }
+    }
+}
